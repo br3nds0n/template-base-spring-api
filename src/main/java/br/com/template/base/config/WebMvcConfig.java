5b72d0b8f20fb2aef11ac60f1f5263b2e1ed9a5e
Brendson Silva <brendson.silva@equipeblue.com.br>
2023-05-05 11:55:14 -0300

    chore: configurando WebMvc

---
+++ src/main/java/br/com/template/base/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package br.com.template.base.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${url.web}")
+    private String origin;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(origin)
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
