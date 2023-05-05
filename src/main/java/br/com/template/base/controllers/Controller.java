8d6fdccbec170080b6c8e1bd2c48d0307490a891
Brendson Silva <brendson.silva@equipeblue.com.br>
2023-05-05 11:33:14 -0300

    chore: configurando swagger

--- src/main/java/br/com/template/base/controllers/Controller.java
+++ src/main/java/br/com/template/base/controllers/Controller.java
@@ -1,5 +1,6 @@
 package br.com.template.base.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
     @GetMapping
+    @Operation(tags = "Auth", summary = "sumario")
     public String app() {
         return "Template Base Running...";
     }
